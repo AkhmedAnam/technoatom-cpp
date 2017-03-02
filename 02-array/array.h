b0c77c37a4af9fe13d4c32b75da724acf6f450db
Victor Tyunyakov <tvs.private@gmail.com>
2017-03-02 23:43:47 +0300

    Added array header file

---
+++ 02-array/array.h
@@ -0,0 +1,74 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+
+class Array
+{
+public:
+  typedef float value_type;
+
+  Array();
+  explicit Array(std::size_t max_size);
+  ~Array();
+  Array(const Array &) = delete;
+  const Array& operator=(const Array& that) = delete;
+
+  //! Returns a reference to the element at specified locationpos,
+  //! with bounds checking.
+  //! If pos is not within the range of the array,
+  //! an exception of type std::out_of_range is thrown.
+  //!
+  //! @param pos is position of the element to return
+  //! @return reference of the element to return
+  value_type& operator[](const std::size_type pos) const;
+
+  //! Returns a reference to the first element in the array.
+  //! Calling front on an empty array is undefined.
+  //!
+  //! @return reference to the first element
+  value_type front() const;
+
+  //! Returns a reference to the last element in the array.
+  //! Calling back on an empty array is undefined.
+  //!
+  //! @return reference to the last element
+  value_type back() const;
+
+  //! Returns pointer to the underlying array serving as element storage.
+  //! The pointer is such that range
+  //! [data(); data() + size()) is always a valid range,
+  //! even if the array is empty
+  //! (data() is not dereferenceable in that case).
+  //!
+  //! @return pointer to the underlying element storage
+  value_type* data() const;
+
+  //! Checks if the array has no elements
+  //!
+  //! @return true if the array is empty, false otherwise
+  bool empty() const;
+
+  //! Returns the number of elements in the array
+  //!
+  //! @return the number of elements in the array
+  std::size_t size() const;
+
+  //! Returns the maximum number of elements the array
+  //! is able to hold due to system or library implementation limitations
+  //!
+  //! @return maximum number of elements
+  std::size_t max_size() const;
+
+  //! Assigns the given value value to all elements in the array.
+  //!
+  //! @param value is the value to assign to the elements
+  void fill(const value_type& value);
+
+  //! Exchanges the contents of the array with those of other.
+  //! Does not cause references to associate with the other array.
+  //!
+  //! @param that is array to exchange the contents with
+  void swap(const Array& that);
+};
+
+#endif // ARRAY_H
